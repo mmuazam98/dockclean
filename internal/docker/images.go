40089738afa26ab6e7106c50fc54a1b155a6dd91
DeexithParand2k2 <deexith2002@gmail.com>
2024-10-07 03:10:08 +0530

    feat : added verbose mode basic functionality

--- internal/docker/images.go
+++ internal/docker/images.go
@@ -49,6 +49,53 @@ func (d *DockerClient) PrintUnusedImages() {
 	}
 }
 
+// VerboseModeCleanup gives more details while doing the cleanup of unused images
+func (d *DockerClient) VerboseModeCleanup() {
+
+	images, err := d.ListUnusedImages()
+	if err != nil {
+		log.Fatalf("Error listing images: %v", err)
+	}
+
+	opts := image.RemoveOptions{Force: true}
+
+	if len(images) == 0 {
+		log.Println("No unused images found")
+		return
+	}
+
+	log.Printf("Found %d unused images. Starting removal in verbose mode...\n", len(images))
+
+	// Print table header
+	fmt.Println("---------------------------------------------------------------")
+	fmt.Printf("%-15s %-12s %-15s %s\n", "ID", "Size (bytes)", "Created (Unix)", "Labels")
+	fmt.Println("---------------------------------------------------------------")
+
+	// Iterate over each unused image and attempt removal
+	for _, image := range images {
+		// Remove the image
+		_, err := d.CLI.ImageRemove(context.Background(), image.ID, opts)
+		if err != nil {
+			log.Printf("Failed to remove image %s: %v\n", image.ID, err)
+		} else {
+			// Print image information in a table-like format
+			fmt.Printf("%-15s %-12d %-15d ", image.ID[:12], image.Size, image.Created)
+
+			// Display labels
+			if len(image.Labels) > 0 {
+				labelStr := ""
+				for key, value := range image.Labels {
+					labelStr += fmt.Sprintf("%s:%s, ", key, value)
+				}
+				fmt.Println(labelStr[:len(labelStr)-2]) // Remove the last comma
+			} else {
+				fmt.Println("No labels")
+			}
+		}
+	}
+
+}
+
 // RemoveUnusedImages deletes unused Docker images
 func (d *DockerClient) RemoveUnusedImages() {
 
@@ -65,6 +112,7 @@ func (d *DockerClient) RemoveUnusedImages() {
 			log.Printf("Failed to remove image %s: %v", image.ID, err)
 		} else {
 			log.Printf("Successfully removed image %s", image.ID)
+
 		}
 	}
 }
