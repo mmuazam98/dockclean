598a28f027f984bca6f91dd4cf08358552ade372
DeexithParand2k2 <deexith2002@gmail.com>
2024-10-30 21:03:26 +0530

    fix : running dockclean command without any flag defaultly points to sizelimit instead of default

--- pkg/utils/flags.go
+++ pkg/utils/flags.go
@@ -39,7 +39,7 @@ func ParseFlags() *Flags {
 	flag.BoolVar(&f.DryRun, "dry-run", false, "List unused Docker images without deleting them")
 	flag.BoolVar(&f.RemoveStopped, "remove-stopped", false, "Remove Images Associated with Stopped Containers")
 	flag.BoolVar(&f.VerboseMode, "verbose", false, "Verbose mode provides additional details about each image during cleanup")
-	flag.Float64Var(&f.SizeLimit.Value, "size-limit", 0, "Specify the size limit to filter images (e.g., 500MB, 1GB)")
+	flag.Float64Var(&f.SizeLimit.Value, "size-limit", -1, "Specify the size limit to filter images (e.g., 500MB, 1GB)")
 	flag.BoolVar(&f.B, "B", false, "Specify the size unit as bytes")
 	flag.BoolVar(&f.KB, "KB", false, "Specify the size unit as kilobytes")
 	flag.BoolVar(&f.MB, "MB", false, "Specify the size unit as megabytes")
